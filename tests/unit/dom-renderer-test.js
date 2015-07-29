5fb855fb2fd0c6ea453e3472f295a10bb7230be2
Matthew Beale <matt.beale@madhatted.com>
2015-07-29 11:30:08 -0400

    Support mobiledoc with version

--- tests/unit/dom-renderer-test.js
+++ tests/unit/dom-renderer-test.js
@@ -3,6 +3,7 @@
 const { test } = QUnit;
 
 import Renderer from 'mobiledoc-html-renderer';
+const MOBILEDOC_VERSION = '0.1';
 
 let renderer;
 QUnit.module('Unit: Mobiledoc HTML Renderer', {
@@ -17,10 +18,13 @@ test('it exists', (assert) => {
 });
 
 test('renders an empty mobiledoc', (assert) => {
-  let mobiledoc = [
-    [], // markers
-    []  // sections
-  ];
+  let mobiledoc = {
+    version: MOBILEDOC_VERSION,
+    sections: [
+      [], // markers
+      []  // sections
+    ]
+  };
   let rendered = renderer.render(mobiledoc);
 
   assert.ok(rendered, 'renders output');
@@ -28,76 +32,91 @@ test('renders an empty mobiledoc', (assert) => {
 });
 
 test('renders a mobiledoc without markers', (assert) => {
-  let mobiledoc = [
-    [], // markers
-    [   // sections
-      [1, 'P', [
-        [[], 0, 'hello world']]
+  let mobiledoc = {
+    version: MOBILEDOC_VERSION,
+    sections: [
+      [], // markers
+      [   // sections
+        [1, 'P', [
+          [[], 0, 'hello world']]
+        ]
       ]
     ]
-  ];
+  };
   let rendered = renderer.render(mobiledoc);
   assert.equal(rendered,
                '<div><p>hello world</p></div>');
 });
 
 test('renders a mobiledoc with simple (no attributes) marker', (assert) => {
-  let mobiledoc = [
-    [        // markers
-      ['B'],
-    ],
-    [        // sections
-      [1, 'P', [
-        [[0], 1, 'hello world']]
+  let mobiledoc = {
+    version: MOBILEDOC_VERSION,
+    sections: [
+      [        // markers
+        ['B'],
+      ],
+      [        // sections
+        [1, 'P', [
+          [[0], 1, 'hello world']]
+        ]
       ]
     ]
-  ];
+  };
   let rendered = renderer.render(mobiledoc);
   assert.equal(rendered, '<div><p><b>hello world</b></p></div>');
 });
 
 test('renders a mobiledoc with complex (has attributes) marker', (assert) => {
-  let mobiledoc = [
-    [        // markers
-      ['A', ['href', 'http://google.com']],
-    ],
-    [        // sections
-      [1, 'P', [
-          [[0], 1, 'hello world']
-      ]]
+  let mobiledoc = {
+    version: MOBILEDOC_VERSION,
+    sections: [
+      [        // markers
+        ['A', ['href', 'http://google.com']],
+      ],
+      [        // sections
+        [1, 'P', [
+            [[0], 1, 'hello world']
+        ]]
+      ]
     ]
-  ];
+  };
   let rendered = renderer.render(mobiledoc);
   assert.equal(rendered, '<div><p><a href="http://google.com">hello world</a></p></div>');
 });
 
 test('renders a mobiledoc with multiple markups in a section', (assert) => {
-  let mobiledoc = [
-    [        // markers
-      ['B'],
-      ['I']
-    ],
-    [        // sections
-      [1, 'P', [
-        [[0], 0, 'hello '], // b
-        [[1], 0, 'brave '], // b+i
-        [[], 1, 'new '], // close i
-        [[], 1, 'world'] // close b
-      ]]
+  let mobiledoc = {
+    version: MOBILEDOC_VERSION,
+    sections: [
+      [        // markers
+        ['B'],
+        ['I']
+      ],
+      [        // sections
+        [1, 'P', [
+          [[0], 0, 'hello '], // b
+          [[1], 0, 'brave '], // b+i
+          [[], 1, 'new '], // close i
+          [[], 1, 'world'] // close b
+        ]]
+      ]
     ]
-  ];
+  };
   let rendered = renderer.render(mobiledoc);
   assert.equal(rendered, '<div><p><b>hello <i>brave new </i>world</b></p></div>');
 });
 
 test('renders a mobiledoc with multiple markups in a section', (assert) => {
   let url = "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACwAAAAAAQABAAACAkQBADs=";
-  let mobiledoc = [
-    [],      // markers
-    [        // sections
-      [2, url]
+  let mobiledoc = {
+    version: MOBILEDOC_VERSION,
+    sections: [
+      [],      // markers
+      [        // sections
+        [2, url]
+      ]
     ]
-  ];
+  };
   let rendered = renderer.render(mobiledoc);
   assert.equal(rendered, `<div><img src="${url}"></div>`);
 });
@@ -108,12 +127,15 @@ test('renders a mobiledoc with card section and src in payload to image', (asser
   let payload = {
     src: 'bob.gif'
   };
-  let mobiledoc = [
-    [],      // markers
-    [        // sections
-      [10, cardName, payload]
+  let mobiledoc = {
+    version: MOBILEDOC_VERSION,
+    sections: [
+      [],      // markers
+      [        // sections
+        [10, cardName, payload]
+      ]
     ]
-  ];
+  };
   let rendered = renderer.render(mobiledoc);
   assert.equal(rendered, '<div><img src="bob.gif"></div>');
 });
@@ -124,12 +146,15 @@ test('renders a mobiledoc with card section and no src to nothing', (assert) => 
   let payload = {
     name: 'bob'
   };
-  let mobiledoc = [
-    [],      // markers
-    [        // sections
-      [10, cardName, payload]
+  let mobiledoc = {
+    version: MOBILEDOC_VERSION,
+    sections: [
+      [],      // markers
+      [        // sections
+        [10, cardName, payload]
+      ]
     ]
-  ];
+  };
   let rendered = renderer.render(mobiledoc);
   assert.equal(rendered, '<div><p></p></div>');
 });
